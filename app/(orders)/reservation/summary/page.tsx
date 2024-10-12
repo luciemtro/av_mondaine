2619c8379c8836bad791e059609d94c575d5034c
Lucie monteiro <lucie.mtro@gmail.com>
2024-10-12 16:29:57 +0200

    add style

--- app/(orders)/reservation/summary/page.tsx
+++ app/(orders)/reservation/summary/page.tsx
@@ -14,57 +14,102 @@ export default function Summary() {
     : [];
 
   const handlePayment = () => {
-    // Assurer que toutes les données sont converties en chaînes de caractères et qu'il n'y a pas de valeurs undefined ou null
     const queryParams = new URLSearchParams(
       Object.entries({
-        totalFee: formData.totalFee?.toString() || "0", // Assurer que totalFee est bien une chaîne
-        firstName: formData.firstName || "", // Valeur par défaut si manquant
-        lastName: formData.lastName || "", // Valeur par défaut
-        email: formData.email || "", // Valeur par défaut
-        phone: formData.phone || "", // Valeur par défaut
-        eventAddress: formData.eventAddress || "", // Valeur par défaut
-        eventCity: formData.eventCity || "", // Valeur par défaut
-        eventPostalCode: formData.eventPostalCode || "", // Valeur par défaut
-        eventCountry: formData.eventCountry || "", // Valeur par défaut
-        eventDate: formData.eventDate || "", // Valeur par défaut
-        eventHour: formData.eventHour?.toString() || "", // Valeur par défaut
-        numberOfPeople: formData.numberOfPeople?.toString() || "0", // Conversion en chaîne de caractères
-        serviceType: formData.serviceType || "", // Valeur par défaut
-        budget: formData.budget?.toString() || "0", // Conversion en chaîne de caractères
-        comment: formData.comment || "", // Valeur par défaut
-        selectedArtists: JSON.stringify(selectedArtists), // Sérialiser en JSON
+        totalFee: formData.totalFee?.toString() || "0",
+        firstName: formData.firstName || "",
+        lastName: formData.lastName || "",
+        email: formData.email || "",
+        phone: formData.phone || "",
+        eventAddress: formData.eventAddress || "",
+        eventCity: formData.eventCity || "",
+        eventPostalCode: formData.eventPostalCode || "",
+        eventCountry: formData.eventCountry || "",
+        eventDate: formData.eventDate || "",
+        eventHour: formData.eventHour?.toString() || "",
+        numberOfPeople: formData.numberOfPeople?.toString() || "0",
+        serviceType: formData.serviceType || "",
+        budget: formData.budget?.toString() || "0",
+        comment: formData.comment || "",
+        selectedArtists: JSON.stringify(selectedArtists),
       })
     ).toString();
 
-    // Rediriger vers la page de paiement avec les query parameters encodés
     router.push(`/reservation/payment?${queryParams}`);
   };
 
   return (
     <div className="pt-28 flex justify-center flex-col summary-container">
-      <h1 className="text-center text-xl">Récapitulatif de la réservation</h1>
-      <div className="golden-border m-10 p-10 flex flex-col gap-2">
-        <p>Prénom: {formData.firstName}</p>
-        <p>Nom: {formData.lastName}</p>
-        <p>Email: {formData.email}</p>
-        <p>Téléphone: {formData.phone}</p>
-        <p>Adresse de l'événement: {formData.eventAddress}</p>
-        <p>Ville de l'événement: {formData.eventCity}</p>
-        <p>Code postal de l'événement: {formData.eventPostalCode}</p>
-        <p>Pays de l'événement: {formData.eventCountry}</p>
-        <p>Date de l'événement: {formData.eventDate}</p>
-        <p>Heure de l'événement: {formData.eventHour}</p>
-        <p>Nombre de personnes: {formData.numberOfPeople}</p>
-        <p>Type de service: {formData.serviceType}</p>
-        <h3>Artistes sélectionnés :</h3>
-        <ul>
-          {selectedArtists.map((artist: { id: number; pseudo: string }) => (
-            <li key={artist.id} className="golden-text">
-              {artist.pseudo}
-            </li>
-          ))}
-        </ul>
-        <p>Montant total: {formData.totalFee} €</p>
+      <h1 className="text-center text-xl golden-text">
+        Récapitulatif de la réservation
+      </h1>
+      <div className="golden-border m-10 p-10 flex flex-col gap-4">
+        <div>
+          <p className="golden-text">Prénom :</p>
+          <p>{formData.firstName}</p>
+        </div>
+        <div>
+          <p className="golden-text">Nom :</p>
+          <p>{formData.lastName}</p>
+        </div>
+        <div>
+          <p className="golden-text">Email :</p>
+          <p>{formData.email}</p>
+        </div>
+        <div>
+          <p className="golden-text">Téléphone :</p>
+          <p>{formData.phone}</p>
+        </div>
+        <div>
+          <p className="golden-text">Adresse de l'événement :</p>
+          <p>{formData.eventAddress}</p>
+        </div>
+        <div>
+          <p className="golden-text">Ville de l'événement :</p>
+          <p>{formData.eventCity}</p>
+        </div>
+        <div>
+          <p className="golden-text">Code postal de l'événement :</p>
+          <p>{formData.eventPostalCode}</p>
+        </div>
+        <div>
+          <p className="golden-text">Pays de l'événement :</p>
+          <p>{formData.eventCountry}</p>
+        </div>
+        <div>
+          <p className="golden-text">Date de l'événement :</p>
+          <p>{formData.eventDate}</p>
+        </div>
+        <div>
+          <p className="golden-text">Heure de l'événement :</p>
+          <p>{formData.eventHour}</p>
+        </div>
+        <div>
+          <p className="golden-text">Nombre de personnes :</p>
+          <p>{formData.numberOfPeople}</p>
+        </div>
+        <div>
+          <p className="golden-text">Type de service :</p>
+          <p>{formData.serviceType}</p>
+        </div>
+        <div>
+          <p className="golden-text">Artistes sélectionnés :</p>
+          <ul>
+            {selectedArtists.map((artist: { id: number; pseudo: string }) => (
+              <li key={artist.id} className="golden-text">
+                {artist.pseudo}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p className="golden-text">Commentaire :</p>
+          <p>{formData.comment || "Aucun commentaire"}</p>
+        </div>
+        <div>
+          <p className="golden-text">Montant total :</p>
+          <p>{formData.totalFee} €</p>
+        </div>
         <button onClick={handlePayment} className="">
           Procéder au paiement
         </button>
